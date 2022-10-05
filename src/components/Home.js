e8b2513eac287336687a8aad7644f9a71a622afe
John Kibirige <kibirigejohn005@gmail.com>
2022-10-05 09:35:53 +0300

    conditionally render corresponding card components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,49 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { fetchSingleData } from './redux/pollution';
+import { fetchData } from '../redux/pollution';
+import Common from './Common';
+import HomeCard from './HomeCard';
 
 const Home = () => {
   const { data, status } = useSelector((state) => state.pollutionData);
   const dispatch = useDispatch();
+  console.log('the data at this stage is ', status, data);
 
   useEffect(() => {
-    for (let obj of data) {
-      dispatch(fetchSingleData(obj));
+    if (status === 'idle') {
+      dispatch(fetchData());
     }
   }, []);
+
   return (
     <div>
-      <Link to={'details'}>DETAILS</Link>
+      {status === 'succeeded' &&
+        data.map((value, index) => {
+          const { country, imageUrl } = value;
+          const { list } = value.fetched;
+          if (index === 0) {
+            return (
+              <Link to={'details'}>
+                <Common
+                  imageUrl={imageUrl}
+                  country={country}
+                  aqIndex={list[0].main.aqi}
+                />
+              </Link>
+            );
+          } else {
+            return (
+              <Link to={'details'}>
+                <HomeCard
+                  imageUrl={imageUrl}
+                  country={country}
+                  aqIndex={list[0].main.aqi}
+                />
+              </Link>
+            );
+          }
+        })}
     </div>
   );
 };
