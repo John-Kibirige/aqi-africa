dbde54a8f5e913e946c96d5c59b828da32e56f57
John Kibirige <kibirigejohn005@gmail.com>
2022-10-06 11:15:52 +0300

    Create local state to monitor any changes in the input field

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchData } from '../redux/pollution';
-import Common from './Common';
+import Search from './Search';
 import HomeCard from './HomeCard';
 
 const Home = () => {
@@ -16,35 +16,45 @@ const Home = () => {
     }
   }, []);
 
+  // search
+  const [searchState, setSearchState] = useState('');
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    setSearchState(value);
+  };
+
   const sequencyForAddingColors = [
     1, 4, 5, 8, 9, 12, 13, 16, 17, 20, 21, 24, 25, 28, 29, 32, 33, 36, 37, 40,
     41, 44, 45, 48, 49, 52, 53, 56, 57, 60,
   ];
 
   return (
-    <div className="card-wrapper grid grid-cols-2 gap-3 p-4">
-      {status === 'succeeded' &&
-        data.map((value, index) => {
-          const { country, imgUrl, id } = value;
-          const { list } = value.fetched;
-          return (
-            <Link
-              to={`details/${id}`}
-              className={`${
-                sequencyForAddingColors.includes(index + 1)
-                  ? 'bg-slate-300'
-                  : 'bg-slate-200'
-              } shadow-lg rounded p-2`}>
-              <HomeCard
-                imageUrl={imgUrl}
-                country={country}
-                aqIndex={list[0].main.aqi}
-                key={id}
-              />
-            </Link>
-          );
-        })}
-    </div>
+    <section className="home-section p-4">
+      <Search handleOnChange={handleOnChange} nameState={searchState} />
+      <div className="card-wrapper grid grid-cols-2 gap-3 pt-4">
+        {status === 'succeeded' &&
+          data.map((value, index) => {
+            const { country, imgUrl, id } = value;
+            const { list } = value.fetched;
+            return (
+              <Link
+                to={`details/${id}`}
+                className={`${
+                  sequencyForAddingColors.includes(index + 1)
+                    ? 'bg-slate-300'
+                    : 'bg-slate-200'
+                } shadow-lg rounded p-2`}>
+                <HomeCard
+                  imageUrl={imgUrl}
+                  country={country}
+                  aqIndex={list[0].main.aqi}
+                  key={id}
+                />
+              </Link>
+            );
+          })}
+      </div>
+    </section>
   );
 };
 
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import searchIcon from '../assets/search-icon.svg';
 
-const Search = () => {
+const Search = ({nameState, handleOnChange}) => {
   return (
     <form className="search-form flex">
       <input
-        className="bg-gray-100 appearance-none border-2 border-pink-300  w-3/4 py-1 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-pink-500"
+        className="bg-gray-100 appearance-none border-2 border-pink-400 w-3/4 py-1 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-pink-500"
         type="text"
-        placeholder="Search by country"></input>
+        placeholder="Search by country or city"
+        value={nameState}
+        name={nameState}
+        onChange={handleOnChange}></input>
       <button className="submit flex bg-pink-600 w-1/4 pl-1 align-middle">
         <img src={searchIcon} alt="search" className="w-5" />{' '}
         <p className="ml-2 text-white">Search</p>
