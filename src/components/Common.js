31c70dc0c200d52ee314f611864bfce4adf05728
John Kibirige <kibirigejohn005@gmail.com>
2022-10-06 17:29:18 +0300

    Refactor Common.js

--- src/components/Common.js
+++ src/components/Common.js
@@ -1,21 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import backChevron from '../assets/back-chevron.svg';
 
 const Common = (props) => {
-  const { imageUrl, country, aqIndex } = props;
+  const { isDetail = false } = props;
   return (
-    <div className="flex bg-pink-500 items-center col-span-2">
-      <div className="image-container bg-">
-        <img
-          src={imageUrl}
-          alt={`${country}`}
-          className="common-image w-52 bg-blend-overlay"
-        />
-      </div>
-      <div className="common-text">
-        <h1 className="font-bold text-2xl uppercase text-white">{country}</h1>
-        <p className="text-white">Air quality index: {aqIndex}</p>
-      </div>
-    </div>
+    <header className="header">
+      <nav className="nav flex justify-center relative items-center">
+        {isDetail && (
+          <Link to={'/'}>
+            <img
+              src={backChevron}
+              alt="back-arrow"
+              className="chevron w-7 hover:border border-solid border-pink-600 p-1 "
+            />
+          </Link>
+        )}
+        <h2 className="py-5 text-pink-500 font-bold text-xl w-10/12 text-center">
+          {isDetail ? 'Details' : 'Home'}
+        </h2>
+      </nav>
+    </header>
   );
 };
 
