32059a5c0800491eedadf138e827462892405c5d
John Kibirige <kibirigejohn005@gmail.com>
2022-10-06 12:51:27 +0300

    use the global state to update local state changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,21 @@ import { useSelector, useDispatch } from 'react-redux';
 import { fetchData } from '../redux/pollution';
 import Search from './Search';
 import HomeCard from './HomeCard';
+import stringComparator from '../script';
+import { updateRecentSearch } from '../redux/pollution';
 
 const Home = () => {
-  const { data, status } = useSelector((state) => state.pollutionData);
+  const { data, status, recentSearch } = useSelector(
+    (state) => state.pollutionData
+  );
   const dispatch = useDispatch();
-  console.log('the data at this stage is ', status, data);
+  console.log(
+    'the data at this stage is ',
+    status,
+    data,
+    ' the recent search ',
+    recentSearch
+  );
 
   useEffect(() => {
     if (status === 'idle') {
@@ -17,11 +27,16 @@ const Home = () => {
   }, []);
 
   // search
-  const [searchState, setSearchState] = useState('');
+  const [searchState, setSearchState] = useState(recentSearch);
   const handleOnChange = (e) => {
     const { value } = e.target;
     setSearchState(value);
   };
+
+  // update recent search
+  useEffect(() => {
+    dispatch(updateRecentSearch(searchState));
+  }, [searchState]);
 
   const sequencyForAddingColors = [
     1, 4, 5, 8, 9, 12, 13, 16, 17, 20, 21, 24, 25, 28, 29, 32, 33, 36, 37, 40,
@@ -31,28 +46,57 @@ const Home = () => {
   return (
     <section className="home-section p-4">
       <Search handleOnChange={handleOnChange} nameState={searchState} />
+
       <div className="card-wrapper grid grid-cols-2 gap-3 pt-4">
-        {status === 'succeeded' &&
-          data.map((value, index) => {
-            const { country, imgUrl, id } = value;
-            const { list } = value.fetched;
-            return (
-              <Link
-                to={`details/${id}`}
-                className={`${
-                  sequencyForAddingColors.includes(index + 1)
-                    ? 'bg-slate-300'
-                    : 'bg-slate-200'
-                } shadow-lg rounded p-2`}>
-                <HomeCard
-                  imageUrl={imgUrl}
-                  country={country}
-                  aqIndex={list[0].main.aqi}
-                  key={id}
-                />
-              </Link>
-            );
-          })}
+        {status === 'succeeded' && searchState
+          ? data
+              .filter((obj) => {
+                const { country } = obj;
+                if (searchState) {
+                  return stringComparator(searchState, country);
+                }
+                return false;
+              })
+              .map((value, index) => {
+                const { country, imgUrl, id } = value;
+                const { list } = value.fetched;
+                return (
+                  <Link
+                    to={`details/${id}`}
+                    className={`${
+                      sequencyForAddingColors.includes(index + 1)
+                        ? 'bg-slate-300'
+                        : 'bg-slate-200'
+                    } shadow-lg rounded p-2`}>
+                    <HomeCard
+                      imageUrl={imgUrl}
+                      country={country}
+                      aqIndex={list[0].main.aqi}
+                      key={id}
+                    />
+                  </Link>
+                );
+              })
+          : data.map((value, index) => {
+              const { country, imgUrl, id } = value;
+              const { list } = value.fetched;
+              return (
+                <Link
+                  to={`details/${id}`}
+                  className={`${
+                    sequencyForAddingColors.includes(index + 1)
+                      ? 'bg-slate-300'
+                      : 'bg-slate-200'
+                  } shadow-lg rounded p-2`}>
+                  <HomeCard
+                    imageUrl={imgUrl}
+                    country={country}
+                    aqIndex={list[0].main.aqi}
+                    key={id}
+                  />
+                </Link>
+              );
+            })}
       </div>
     </section>
   );
