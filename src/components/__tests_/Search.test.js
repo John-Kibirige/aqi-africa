c39c35c8bcffff8f90268de01efb5ff457afd60c
John Kibirige <kibirigejohn005@gmail.com>
2022-10-08 13:56:00 +0300

    Add tests for the Search compoenent

--- src/components/__tests_/Search.test.js
+++ src/components/__tests_/Search.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { screen, render } from '@testing-library/react';
 import Search from '../Search';
 
-if('Renders when there are no props passed to it', () => {
-    const tree = renderer.create(<Search />).toJSON();
-    expect(tree).toMatchSnapshot();
-})
+it('Renders when there are no props passed to it', () => {
+  const tree = renderer.create(<Search />).toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+it('Render input field well ', () => {
+  render(<Search />);
+  const inputEl = screen.getByPlaceholderText(/Search by country/i);
+  expect(inputEl).toBeInTheDocument();
+});
